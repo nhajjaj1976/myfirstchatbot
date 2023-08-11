e9bdcca1f573fda63e2d6afa02190b79522c5542
nhajjaj1976 <130145288+nhajjaj1976@users.noreply.github.com>
2023-08-11 23:24:18 +0300

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import openai
+
+# Initialize the GPT-4 API client with the secret key
+openai.api_key = st.secrets["gpt4_api_key"]
+
+def get_gpt4_response(prompt):
+    response = openai.Completion.create(engine="text-davinci-004", prompt=prompt, max_tokens=150)
+    return response.choices[0].text.strip()
+
+st.title("My GPT-4 Chatbot")
+
+user_input = st.text_input("Ask me anything!")
+if user_input:
+    response = get_gpt4_response(user_input)
+    st.write(f'Chatbot: {response}')
